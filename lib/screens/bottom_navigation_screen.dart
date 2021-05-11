bb9a878cb835ff00c7f99799682714d8970a74f9
tai0116 <taisukekacho@gmail.com>
2021-05-11 20:06:11 +0900

    スクロールできるようになった！

--- lib/screens/bottom_navigation_screen.dart
+++ lib/screens/bottom_navigation_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NavBottom extends StatefulWidget {
@@ -12,15 +13,21 @@ class _NavBottomState extends State<NavBottom> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Divider(
-            color: Colors.black,
-            indent: 32.0,
-            endIndent: 32.0,
-          ),
-          Text(widget.title),
-        ],
+      body: Container(
+        width: double.infinity,
+        child: GridView.count(
+          crossAxisCount: 1,
+          children: List.generate(100, (index) {
+            return InkWell(
+              onTap: () {},
+              child: Center(
+                child: Text(
+                  'Item $index',
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
